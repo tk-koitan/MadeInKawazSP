1119b1e94dd4934f9e93c01107d378547d4caa13
Zakky3 <zakky0317@eis.hokudai.ac.jp>
2020-12-12 18:25:56 +0900

    もぐらのみきり書きかけ

--- MadeInKawazSP/Assets/Zakky/Games/SetsunaMogura/Scripts/Mogura.cs
+++ MadeInKawazSP/Assets/Zakky/Games/SetsunaMogura/Scripts/Mogura.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Mogura : MonoBehaviour
 {
+    float PyonTime;
+    ZakkyLib.Timer mMoguraTimer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PyonTime = Random.Range(1f, 3f);
+        mMoguraTimer = new ZakkyLib.Timer(PyonTime);
     }
 
     // Update is called once per frame
