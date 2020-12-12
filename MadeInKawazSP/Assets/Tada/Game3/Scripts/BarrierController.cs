2b1d42d11e64a9177723474765d3826200bab278
tada <tatata0389@gmail.com>
2020-12-12 18:12:07 +0900

    TadaGame3を大体作った

---
+++ MadeInKawazSP/Assets/Tada/Game3/Scripts/BarrierController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TadaGame3
+{
+    public class BarrierController : MonoBehaviour
+    {
+        [SerializeField]
+        private TouchRotateChecker rotate_checker_;
+
+        [SerializeField]
+        private Vector3 center_position_ = Vector3.zero;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rotate_checker_.Init(center_position_);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 add = new Vector3(0f, 0f, rotate_checker_.GetRotateLength());
+            transform.localEulerAngles += add;
+        }
+    }
+}
---
+++ MadeInKawazSP/Assets/Tada/Game3/Scripts/MeteoriteController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TadaGame3
+{
+    public class MeteoriteController : MonoBehaviour
+    {
+        private float speed_;
+        private Vector3 dir_;
+
+        private bool inited_ = false;
+        private bool destoryed_ = false;
+
+        [SerializeField]
+        private ParticleSystem burst_eff_;
+
+        [SerializeField]
+        private ParticleSystem drop_eff_;
+
+        public void Init(Vector3 spawner_pos, float speed, Vector3 dir)
+        {
+            transform.position = spawner_pos;
+            transform.localEulerAngles = new Vector3(0f, 0f, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 90f);
+
+            speed_ = speed;
+            dir_ = dir;
+            inited_ = true;
+        }
+
+        private void Update()
+        {
+            if (!inited_) return;
+            if (destoryed_) return;
+
+            transform.position += dir_ * Time.deltaTime * speed_;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.tag == "StageObject")
+            {
+                destoryed_ = true;
+                GetComponent<SpriteRenderer>().enabled = false;
+                GetComponent<CircleCollider2D>().enabled = false;
+
+                burst_eff_.gameObject.SetActive(true);
+                drop_eff_.gameObject.SetActive(false);
+            }
+        }
+    }
+}
