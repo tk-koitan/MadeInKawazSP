5c65d6cc70733eacd05fa597f95ee0d63e0fd803
DESKTOP-P97N7L7\tk_ka <tk_kakutasu@yahoo.co.jp>
2020-12-19 16:20:30 +0900

    メダルゲーム追加

---
+++ MadeInKawazSP/Assets/Koitan/CoinPusher/Scripts/Coin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoinPusher
+{
+    public class Coin : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            GameManager.Clear();
+        }
+    }
+}
